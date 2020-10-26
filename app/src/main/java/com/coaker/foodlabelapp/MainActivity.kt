6d8f17fb26322e96abbf1590a6a89e1de1ef7458
sjc7375 <seancoaker@gmail.com>
2020-10-26 11:01:43 +0000

    "Added ML Kit Barcode Scanning Code"

--- app/src/main/java/com/coaker/foodlabelapp/MainActivity.kt
+++ app/src/main/java/com/coaker/foodlabelapp/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.coaker.foodlabelapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.barcode.Barcode
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.common.InputImage
 
 class MainActivity : AppCompatActivity() {
+    val options = BarcodeScannerOptions.Builder()
+        .setBarcodeFormats(Barcode.FORMAT_EAN_13, Barcode.FORMAT_EAN_8).build()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+}
+
+private class YourImageAnalyzer : ImageAnalysis.Analyzer {
+    @SuppressLint("UnsafeExperimentalUsageError")
+    override fun analyze(imageProxy: ImageProxy) {
+        val mediaImage = imageProxy.image
+        if (mediaImage != null) {
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+        }
     }
 }
